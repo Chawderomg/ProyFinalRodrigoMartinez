52e7bbb77a49acc55669036c6357c47bc7377ad6
Chawderomg <rrorro596@gmail.com>
2026-02-03 22:01:15 -0400

    Segundo cambio

--- test/suma.test.js
+++ test/suma.test.js
@@ -1,8 +1,18 @@
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { sumaR } from './src/suma.js'
 
 
-test('sumar 8 + 6 to equal 14', () => {
-  expect(sumaR(8, 6)).toBe(14)
+describe('sum',()=>{
+
+  it('suma dos numeros',()=>{
+    expect(sumaR(8, 6)).toBe(14)
+  })
+
+  it('suma con cero',()=>{
+    expect(sumaR(7, 0)).toBe(7)
+  })
+
 })
+
+
 
